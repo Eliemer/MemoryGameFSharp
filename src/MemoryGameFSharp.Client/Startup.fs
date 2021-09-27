c96819888e3c52cf2147b2b51935268065c73037
Eliemer <eli.rodriguez@enium.com>
2021-09-27 15:17:16 -0400

    init commit

--- src/MemoryGameFSharp.Client/Startup.fs
+++ src/MemoryGameFSharp.Client/Startup.fs
@@ -10,7 +10,6 @@ module Program =
     [<EntryPoint>]
     let Main args =
         let builder = WebAssemblyHostBuilder.CreateDefault(args)
-        builder.RootComponents.Add<Main.MyApp>("#main")
         builder.Services.AddScoped<HttpClient>(fun _ ->
             new HttpClient(BaseAddress = Uri builder.HostEnvironment.BaseAddress)) |> ignore
         builder.Build().RunAsync() |> ignore
